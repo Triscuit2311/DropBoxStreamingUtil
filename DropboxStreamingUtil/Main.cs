ca7311b76e357a7f59813cc9c7941a6b024660ca
Triscuit2311 <70150617+Triscuit2311@users.noreply.github.com>
2021-08-09 21:40:09 -0400

    Initial Commit

---
+++ DropboxStreamingUtil/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Dropbox.Api;
+
+namespace DropboxStreamingUtil
+{
+    public static class Main
+    {
+        public static async Task<byte[]> _Download(string path, string apiToken)
+        { // ...("/Folder/FancyCheat.dll", "PBQVCOaLLgEAAAAAAAAAAaXzmK29rXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" )
+            var dbx = new DropboxClient(apiToken);
+            using (var response = await dbx.Files.DownloadAsync(path))
+            {
+                return (await response.GetContentAsByteArrayAsync());
+            }
+        }
+
+        public static GCHandle PinArray(byte[] arr)
+        {
+            return GCHandle.Alloc(arr, GCHandleType.Pinned);
+        }
+
+        public static IntPtr GetAddressOfPinnedArray(GCHandle gcHandle)
+        {
+            return gcHandle.AddrOfPinnedObject();
+        }
+        
+    }
+}
